43378891b663e6cfb25d1ca47234737974dea563
nghiatrx <dnghia13@gmail.com>
2018-06-20 14:53:49 +0700

    Update payment.js

--- controllers/payment.js
+++ controllers/payment.js
@@ -30,58 +30,56 @@ var paymentController = {
 
             var account = web3.eth.accounts.privateKeyToAccount('0x' +  utils.bufferToStr(privateKey));
 
-            web3.eth.getTransactionCount(account.address, function(err, count){
+            web3.eth.getTransactionCount(account.address, 'pending', function(err, count){
                 
                 if (err) {
                     return res.status(422).json({message: "Can't get sender address info"});
                 }
 
-                Payment.find({status: 'pending', 'from_address': account.address}).count(function(err, numPending) {
-                    const rawTransaction =  {
-                        nonce: count + numPending,
-                        gasLimit: web3.utils.toHex(req.body.gas_limit),
-                        gasPrice: web3.utils.toHex(80e9), // 80 Gwei
-                        to: config.token.contract_address,
-                        value: "0x00",
-                        data: contract.methods.transfer(config.destination_address, req.body.amount * Math.pow(10, config.token.decimals)).encodeABI(),
-                        chainId : config.token.chainId
+                const rawTransaction =  {
+                    nonce: count,
+                    gasLimit: web3.utils.toHex(req.body.gas_limit),
+                    gasPrice: web3.utils.toHex(80e9), // 80 Gwei
+                    to: config.token.contract_address,
+                    value: "0x00",
+                    data: contract.methods.transfer(config.destination_address, req.body.amount * Math.pow(10, config.token.decimals)).encodeABI(),
+                    chainId : config.token.chainId
+                }
+
+                var transaction = new ethTx(rawTransaction)
+
+                transaction.sign(privateKey)
+
+                web3.eth.sendSignedTransaction('0x' + transaction.serialize().toString('hex'), function(err, tx) {
+
+                    if (tx) {
+                        mongoose.connect(config.mongodb)
+
+                        payment = new Payment()
+
+                        payment.tx                  = tx
+                        payment.transaction_id      = req.body.transaction_id
+                        payment.amount              = req.body.amount
+                        payment.to_address          = config.destination_address
+                        payment.gas_limit           = req.body.gas_limit
+                        payment.user_id             = req.body.user_id
+                        payment.status              = 'pending'
+                        payment.try                 = 0
+                        payment.from_address        = account.address
+
+                        payment.save(function(err, post) {
+                            if (err) {
+                                console.log(err);
+                            }
+                            else {
+                                return res.status(200).json(post)
+                            }
+                        })
+
+                    } else {
+                        return res.status(422).json({message: "Can't send tx"})
                     }
-    
-                    var transaction = new ethTx(rawTransaction)
                     
-                    transaction.sign(privateKey)
-    
-                    web3.eth.sendSignedTransaction('0x' + transaction.serialize().toString('hex'), function(err, tx) {
-    
-                        if (tx) {
-                            mongoose.connect(config.mongodb)
-    
-                            payment = new Payment()
-    
-                            payment.tx                  = tx
-                            payment.transaction_id      = req.body.transaction_id
-                            payment.amount              = req.body.amount
-                            payment.to_address          = config.destination_address
-                            payment.gas_limit           = req.body.gas_limit
-                            payment.user_id             = req.body.user_id
-                            payment.status              = 'pending'
-                            payment.try                 = 0
-                            payment.from_address        = account.address
-                            
-                            payment.save(function(err, post) {
-                                if (err) {
-                                    console.log(err);
-                                }
-                                else {
-                                    return res.status(200).json(post)
-                                }
-                            })
-                            
-                        } else {
-                            return res.status(422).json({message: "Can't send tx"})
-                        }
-                    
-                    });
                 })
             })  
 
